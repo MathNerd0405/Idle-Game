965898afcc45bf2af2d9214d3b9519458f14da39
MathNerd0405 <60146807+MathNerd0405@users.noreply.github.com>
2020-01-29 09:31:27 -0600

    Update game.js
    added more sprites

--- game.js
+++ game.js
@@ -7,6 +7,15 @@ game.state.add('play',{
   create: function(){
       var skeletonSprite = game.add.sprite(450, 290, 'skeleton');
       skeletonSprite.anchor.setTo(0.5,0.5);
+      var state =this;
+      
+      this.background = this.game.add.group();
+      ['forest-back', 'forest-lights','forest-middle','forest-front']
+        .forEach(function(image) {
+          var bg = state.game.add.tileSprite(0,0, state.game.world.width,
+            state.game.world.height,image,'', state.background);
+          bg.tileScale.setTo(4,4);
+        });
   },
   render: function(){
       game.debug.text('Adventure Awaits!', 250, 290);
